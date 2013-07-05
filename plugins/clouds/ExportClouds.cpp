2c038efd6c66f329d02e11aeab4082cf86fe40b1
David Koerner <dk402538@googlemail.com>
2013-07-05 18:10:50 +0200

    ExportClouds only saving qv and qc

--- plugins/clouds/ExportClouds.cpp
+++ plugins/clouds/ExportClouds.cpp
@@ -54,25 +54,30 @@ void ExportClouds::update(core::GraphNodeSocket *output)
 
 		for (int i=0; i<subDataNames.size(); ++i)
 		{
+			QString subDataName = subDataNames[i];
+
+			if((subDataName!="qc") && (subDataName!="qv") )
+				continue;
+
 			// ScalarField?
-			if( so->getSubData<ScalarField>(subDataNames[i]) )
+			if( so->getSubData<ScalarField>(subDataName) )
 			{
-				houGeo->addPrimitive(so->getSubData<ScalarField>(subDataNames[i]));
-				nameAttr->addString(subDataNames[i].toStdString());
+				houGeo->addPrimitive(so->getSubData<ScalarField>(subDataName));
+				nameAttr->addString(subDataName.toStdString());
 			}else
 			// VectorField?
-			if( so->getSubData<VectorField>(subDataNames[i]) )
+			if( so->getSubData<VectorField>(subDataName) )
 			{
-				houGeo->addPrimitive(so->getSubData<VectorField>(subDataNames[i])->getScalarField(0));
-				nameAttr->addString((subDataNames[i]+".x").toStdString());
-				houGeo->addPrimitive(so->getSubData<VectorField>(subDataNames[i])->getScalarField(1));
-				nameAttr->addString((subDataNames[i]+".y").toStdString());
-				houGeo->addPrimitive(so->getSubData<VectorField>(subDataNames[i])->getScalarField(2));
-				nameAttr->addString((subDataNames[i]+".z").toStdString());
+				houGeo->addPrimitive(so->getSubData<VectorField>(subDataName)->getScalarField(0));
+				nameAttr->addString((subDataName+".x").toStdString());
+				houGeo->addPrimitive(so->getSubData<VectorField>(subDataName)->getScalarField(1));
+				nameAttr->addString((subDataName+".y").toStdString());
+				houGeo->addPrimitive(so->getSubData<VectorField>(subDataName)->getScalarField(2));
+				nameAttr->addString((subDataName+".z").toStdString());
 
 				for( int j=0;j<3;++j )
 				{
-					ScalarField::Ptr f = so->getSubData<VectorField>(subDataNames[i])->getScalarField(j);
+					ScalarField::Ptr f = so->getSubData<VectorField>(subDataName)->getScalarField(j);
 					math::Box3f b = f->bound();
 					qDebug() << f->getVoxelSize().x << " " << f->getVoxelSize().y << " " << f->getVoxelSize().z;
 				}
@@ -108,22 +113,26 @@ void ExportClouds::update(core::GraphNodeSocket *output)
     }else
         qDebug() << "ExportClouds:: NO so! " << filename;
 
-    /*
+
+	/*
     if(so)
     {
         std::ofstream out( (filename+".log").toUtf8() , std::ios_base::out | std::ios_base::binary );
         houdini::HouGeoIO::makeLog( filename.toStdString(), &out );
     }
-
+	*/
+	/*
     {
         filename = "c:/projects/clouds/bin/null.bgeo";
         std::ofstream out( (filename+".log").toUtf8() , std::ios_base::out | std::ios_base::binary );
         houdini::HouGeoIO::makeLog( filename.toStdString(), &out );
     }
-    {
-        filename = "c:/projects/clouds/bin/test.bgeo";
-        std::ofstream out( (filename+".log").toUtf8() , std::ios_base::out | std::ios_base::binary );
-        houdini::HouGeoIO::makeLog( filename.toStdString(), &out );
-    }
-    */
+	*/
+	/*
+	{
+		filename = "c:/projects/clouds/bin/ref.bgeo";
+		std::ofstream out( (filename+".log").toUtf8() , std::ios_base::out | std::ios_base::binary );
+		houdini::HouGeoIO::makeLog( filename.toStdString(), &out );
+	}
+	*/
 }
